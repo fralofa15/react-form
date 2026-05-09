ad95317ad099bd0d0be8b92c0f6ad5ef02bf4cdd
Lorenzo Favale <lorenzofavale150420@gmail.com>
2026-05-09 15:05:38 +0200

    eliminato article.jsx e spostata la logica in app.jsx aggiunti useState per postare titolo e commento

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import Article from "./components/article";
+import { useState } from "react";
 import Form from "./components/form"
 function App() {
+
+    const [post, setPost] = useState([
+        {
+            id: 1,
+            title: "primo",
+            text: "primo testo"
+        },
+        {
+            id: 2,
+            title: "secondo",
+            text: "secondo testo"
+        }
+    ]);
+
     return (
         <>
-            <Form />
-            <Article />
+            <Form post={post} setPost={setPost}/>
+            <div>
+                {post.map((element) => (
+                    <div key={element.id}>
+                        <h1>{element.title}</h1>
+                        <p>{element.text}</p>
+                    </div>
+                ))
+                }
+            </div>
         </>
     );
 }
--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,14 +1,51 @@
-function Form() {
+import { useState } from "react"
+function Form(props) {
+
+    const [title, setTitle] = useState("")
+    const [text, setText] = useState("")
+
+    function handleTitleChange(event) {
+        setTitle(event.target.value)
+    }
+
+    function handleTextChange(event) {
+        setText(event.target.value)
+    }
+
+    function handleNewPost(event) {
+        event.preventDefault();
+
+        const newPost = {
+            id: props.post.length + 1,
+            title: title,
+            text: text
+        }
+        props.setPost([...props.post, newPost]);
+
+        setTitle("");
+        setText("");
+    }
+
     return (
-        <div>
+        <form>
             <div>
-                <input type="text" placeholder="Titolo" />
+                <input
+                    type="text"
+                    placeholder="Titolo"
+                    value={title}
+                    onChange={handleTitleChange}
+                />
             </div>
             <div>
-                <textarea rows={5} cols={30} placeholder="Commento"></textarea>
+                <textarea
+                    rows={5} cols={30}
+                    placeholder="Commento"
+                    value={text}
+                    onChange={handleTextChange}
+                ></textarea>
             </div>
-            <button>Posta</button>
-        </div>
+            <button onClick={handleNewPost}>Posta</button>
+        </form>
     );
 }
 export default Form
